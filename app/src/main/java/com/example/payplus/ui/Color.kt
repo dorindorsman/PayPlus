a7412c875ae12c16c06312db4cfd84bb501d2f43
Dorin Dorsman <dorin.dorsman@intel.com>
2024-03-05 10:20:56 +0200

    Project setup 2

---
+++ app/src/main/java/com/example/payplus/ui/Color.kt
@@ -0,0 +1,16 @@
+package com.example.payplus.ui
+
+import androidx.compose.ui.graphics.Color
+
+val primaryColor = Color(0xFF4FF46B)
+val primaryLightColor = Color(0xFF8CFF9C)
+val primaryDarkColor = Color(0xFF006D21)
+val primaryTextColor = Color(0xFF000000)
+val secondaryColor = Color(0xFF8E24AA)
+val secondaryLightColor = Color(0xFFC158DC)
+val secondaryDarkColor = Color(0xFF5C007A)
+val secondaryTextColor = Color(0xFFFFFFFF)
+
+val LightGray = Color(0xFFFCFCFC)
+val MediumGray = Color(0xFF9C9C9C)
+val DarkGray = Color(0xFF141414)
